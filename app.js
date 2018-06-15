6f85c20c5dd5696a860284bf4599e74aaf8a30f8
arthurtruong <arthurtruong.work@gmail.com>
2018-06-16 01:18:09 +1000

    consoles

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
-function sayHello() {
-  document.write ("Hello World");
-}
+// function sayHello() {
+//   document.write ("Hello World");
+// }
+//
+// function sayAndrew() {
+//   document.write ("Hey andrew, you fat cunt")
+// }
 
-function sayAndrew() {
-  document.write ("Hey andrew, you fat cunt")
-}
+console.log('Hello World');
+console.log(123);
+console.log(true);
+
+// Variable
+var greeting = 'Hello World - 2';
+console.log(greeting);
+
+// Arrays
+console.log([1,2,3,4,5]);
+
+// Objects
+console.log({a:1, b:2, c:3, d:4});
+
+// Print object on a table
+console.table({a:1, b:2, c:3, d:4});
+
+// How to print errors - this makes it red
+console.error("This is an error test");
+
+// How to clear console
+console.clear();
+
+// How to display warnings - this makes it yellow
+console.warn('This is a warning test');
+
+// How to time something loads - used for optimization
+console.time();
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.timeEnd();
