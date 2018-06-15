b68bd213dd4cd6c80dad9ac5e44296c1b8dfcc44
arthurtruong <arthurtruong.work@gmail.com>
2018-06-16 01:43:07 +1000

    variables

---
+++ variables.js
@@ -0,0 +1,52 @@
+// var, let, const
+
+var myName = 'Christine';
+console.log(myName);
+myName = 'Arthur';
+console.log(myName);
+
+// Init a variable
+
+var greetings
+console.log(greetings);
+greetings = 'Arthur'
+console.log(greetings);
+
+// letters, numbers , _, $ - you can start it
+// cannot start with a number
+
+// Multi variables
+var firstName = 'John'; // Camel case
+var first_name = 'Sarah'; // Underscore
+var FirstName = 'Christine'; // Pascal case
+var firstname;
+
+// let
+let myName;
+myName = 'Christine';
+console.log(myName);
+myName = 'Arthur';
+console.log(myName);
+
+// Const
+const name = 'John';
+console.log(name);
+Cannot reassign
+name = 'sara';
+Have to assign a value
+// const greetings; <-- wont work
+
+const person = {
+  name: 'John',
+  age: 30
+}
+
+person.name = ('Sarah');
+person.age = (32)
+
+console.log(person);
+
+// const Arrays
+const numbers = [1,2,3,4,5];
+numbers.push(6);
+console.log(numbers);
